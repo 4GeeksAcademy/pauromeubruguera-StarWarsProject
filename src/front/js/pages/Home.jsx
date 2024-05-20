18f7ff7490cf43390d52001b375152cbf6c3670f
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-05-20 13:06:17 +0000

    video change

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import Video from '../../video/videoHome.mp4'
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="video-responsive text-center mt-5">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/FoeuhJ8lnEQ?si=DoXC2J5AC2n_iAJ2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; muted" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+			<video src={Video} type="video/mp4" controls="none" muted loop autoplay="true" />
 		</div>
 	);
 };
--- webpack.common.js
+++ webpack.common.js
@@ -32,7 +32,17 @@ module.exports = {
             options: { name: '[name].[ext]' }
           }
         }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
+        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] }, //for fonts
+        {
+          test: /\.(mp4|webm|ogg|avi|mkv)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'videos/'
+            }
+          }
+        }, //for videos
     ]
   },
   resolve: {
