3303c117a4130ae336a32f8a9b8e58b7d5ca94f4
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-06-03 16:50:40 +0000

    models

--- src/api/models.py
+++ src/api/models.py
@@ -9,12 +9,120 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User: {self.email}>'
 
     def serialize(self):
         # Do not serialize the password, its a security breach
-        return {"id": self.id,
-                "email": self.email,
-                "is_active": self.is_active}
+        return {'id': self.id,
+                'email': self.email,
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'last_name': self.last_name}
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author_to = db.relationship('Users', foreign_keys=[author_id])
+    body = db.Column(db.String(600), nullable=False)
+    date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Post: {self.title}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'title': self.title,
+                'author_id': self.author_id,
+                'body': self.body,
+                'date': self.date}
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    author_to = db.relationship('Users', foreign_keys=[author_id])
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post_to = db.relationship('Posts', foreign_keys=[post_id])
+    body = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Comment: {self.body}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'post_id': self.post_id,
+                'author_id': self.author_id,
+                'body': self.body,
+                'date': self.date}
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    diameter = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Planet: {self.name}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'name': self.name,
+                'diameter': self.diameter}
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    home_world = db.Column(db.String, db.ForeignKey('planets.id'))
+    planet_to = db.relationship('Planets', foreign_keys=[home_world])
+    description = db.Column(db.String(300))
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Character: {self.name}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'home_world': self.home_world,
+                'description': self.description,
+                'name': self.name}
+
+class Films(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    release = db.Column(db.DateTime)
+    director = db.Column(db.String())
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Film: {self.name}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'release': self.release,
+                'director': self.director,
+                'name': self.name}
+
+class CharacterFilms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.id'))
+    film_to = db.relationship('Films', foreign_keys=[film_id])
+    character_id = db.Column(db.String, db.ForeignKey('characters.id'))
+    character_to = db.relationship('Characters', foreign_keys=[character_id])
+    role = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<CharacterFilm: {self.role}>'
+
+    def serialize(self):
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'film_id': self.film_id,
+                'character_id': self.character_id,
+                'role': self.role}
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Posts, Comments, Planets, Characters, Films, CharacterFilms
 
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,60 @@ def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return response_body, 200
+
+
+@api.route('/users', methods=['GET, POST'])
+def handle_users():
+    response_body = {}
+    if request.method == 'GET':
+        # Aqui tengo que hacer la logica para mostrar los usuarios que tengo en mi DB
+        users = db.session.execute(db.select(Users)).scalars()
+        users_list = [row.serialize() for row in users]  # Utilizo list comprehension
+        response_body['data'] = users_list
+        response_body['message'] = "Get de users"
+        return response_body, 200
+    if request.method == 'POST':
+        response_body['message'] = "Este end point no es valido. Ud debe hacer un signup"
+        return response_body, 200
+
+
+@api.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_users(id):
+    response_body = {}
+    if request.method == 'GET':
+        users = db.session.execute(db.select(Users).where(Users.id == id)).scalars()
+        if user:
+            response_body['data'] = user.serialize()
+            response_body['message'] = "Usuario encontrado"
+            return response_body, 200
+        response_body['data'] = {}
+        response_body['message'] = "Usuario inexistente"
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        # TODO: validacion datos recibidos
+        user = db.session.execute(db.select(Users).where(Users.id == id)).scalar()
+        if user:
+            user.email = data['email']
+            user.is_active = data['is_active']
+            user.last_name = data['last_name']
+            user.first_name = data['first_name']
+            db.session.commit()
+            response_body['data'] = user.serialize()
+            response_body['message'] = "Datos del usuario actualizados"
+            return response_body, 200
+        response_body['data'] = {}
+        response_body['message'] = "Usuario inexistente"
+        return response_body, 404
+    if request.method == 'DELETE':
+        user = db.session.execute(db.select(Users).where(Users.id == id)).scalar()
+        if user:
+            # db.session.delete(user)
+            user.is_active = False
+            db.session.commit()
+            response_body['data'] = {}
+            response_body['message'] = "Usuario eliminado"
+            return response_body, 200
+        response_body['data'] = {}
+        response_body['message'] = "Usuario inexistente"
+        return response_body, 404
